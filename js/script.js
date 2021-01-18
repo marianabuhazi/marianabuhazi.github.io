48c3c5c0aeb5bee8f67572cf7e0114b42ec306d3
Marian Abuhazi <marian.abuhazi@columbia.edu>
2021-01-18 00:55:12 -0500

    Styled all the way down to blog responsively, need to make the nav bar hide for mobile, need to make the profile responsive and need to make more engaging

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 //Checks that doc is loaded
+alert("Hello! I have only been working on this site for a couple of days, and it is still under construction! For now, have fun getting to know me :)");
 $( document ).ready(function() {
     console.log( "Doc ready!" );
 
@@ -7,6 +8,10 @@ $(".headerPic").on('load', function() {
 //Animates subpic "Marian Abuhazi Interactive..." to be centered based on different screen sizes
 var less600 = window.matchMedia("(max-width: 600px)");
 var more600=window.matchMedia("(min-width: 600px)");
+
+changeAnimate(less600);
+changeAnimate(more600);
+
 function changeAnimate() {
       if (less600.matches) {
         console.log("media query works for < 600px!");
@@ -34,12 +39,7 @@ function changeAnimate() {
       }
    }
 
-changeAnimate(less600);
-changeAnimate(more600);
-
 })
-
-
 
 //Fixes nav to the top and allows it to scroll to different parts of page
   let mainNavLinks = document.querySelectorAll("nav ul li a");
@@ -78,24 +78,87 @@ function showDropdown(){
 }
 
 //Function that when Read More button is clicked, it expands div
-var readMoreBtn= $(".readMore");
+var readMoreBtn= $("#readMore");
+var readLessBtn= $("#readLess");
+var arrows= $(".arrow-animation");
 var rMPar= $(".readMorePar");
 
 function showReadMore(){
-  if(rMPar.css("display")=="none"){
-    rMPar.addClass("showReadMore");
-    readMoreBtn.text("Read Less");
-    $(".ProfileDiv-row").css("align-items", "flex-start");
-    $("#marianSki").css("display", "block");
-  }
-  else{
-    rMPar.removeClass("showReadMore");
-    readMoreBtn.text("Read More About Me");
-    $("#marianSki").css("display", "none");
+  if(rMPar.css("display")=="none")
+  {
+    rMPar.addClass("showStyleReadMore");
+    readMoreBtn.css("display", "none");
+    readLessBtn.css("display", "block");
+    arrows.css("display", "inline-block");
   }
   console.log("Read More Button clicked!");
 }
 
+function showReadLess(){
+    rMPar.removeClass("showStyleReadMore");
+    readLessBtn.css("display", "none");
+    readMoreBtn.css("display", "block");
+    arrows.css("display", "none");
+    console.log("Read Less Button clicked!");
+  }
+
 readMoreBtn.on("click", showReadMore);
+readLessBtn.on("click", showReadLess)
+
+
+
+//function that when More about this role button is clicked, div expands to show images.
+var moreRole= $(".roleBtn");
+var zgbtn1= $("#zgrole1");
+var zgbtn2= $("#zgrole2");
+var zgbtn3= $("#zgrole3");
+
+var zgImage1= $("#showRole-img-1");
+var zgImage2= $("#showRole-img-2");
+var zgImage3= $("#showRole-img-3");
+
+function showMoreRole1(){
+  if(zgImage1.css("display")=="none")
+  {
+    zgbtn1.text("Less about this role");
+    zgImage1.css("display", "block");
+  }
+  else{
+    zgbtn1.text("More about this role");
+    zgImage1.css("display", "none");
+  }
+  console.log("More about this role Button 1 clicked!");
+}
+
+function showMoreRole2(){
+  if(zgImage2.css("display")=="none")
+  {
+    zgbtn2.text("Less about this role");
+    zgImage2.css("display", "block");
+  }
+  else{
+    zgbtn2.text("More about this role");
+    zgImage2.css("display", "none");
+  }
+  console.log("More about this role Button 1 clicked!");
+}
+
+function showMoreRole3(){
+  if(zgImage3.css("display")=="none")
+  {
+    zgbtn3.text("Less about this role");
+    zgImage3.css("display", "block");
+  }
+  else{
+    zgbtn3.text("More about this role");
+    zgImage3.css("display", "none");
+  }
+  console.log("More about this role Button 1 clicked!");
+}
+
+
+zgbtn1.on("click", showMoreRole1);
+zgbtn2.on("click", showMoreRole2);
+zgbtn3.on("click", showMoreRole3);
 
 });
